a1ea9665509e524c7794a5920fef56cf70d27a0f
Gino Grippo <gino.carlo02@gmail.com>
2023-11-23 14:01:51 -0300

    Restyle

--- Assets/Scripts/ChooseLanguage.cs
+++ Assets/Scripts/ChooseLanguage.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ChooseLanguage : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] btnLanguageGameObjects;
+
+
     public void Language(string language)
     {
         GameManager.instance.setLanguague(language);
+    }
+
+    public void ChangeBtnUI(int language)
+    {
+        DisableAllLanguageButtons();
+        btnLanguageGameObjects[language].SetActive(true);
+    }
+
+    public void DisableAllLanguageButtons()
+    {
+        for(int i = 0; i < btnLanguageGameObjects.Length; i++)
+        {
+            btnLanguageGameObjects[i].SetActive(false); 
+        }
     }
 }
